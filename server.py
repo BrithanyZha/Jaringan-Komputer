4f03ef9e19a0e2a4a8f47bcbfcb23fa0800319f2
Brithany Zhafira N <122023846+BrithanyZha@users.noreply.github.com>
2023-05-07 15:13:42 +0700

    Update server.py

--- server.py
+++ server.py
@@ -17,24 +17,29 @@ server_socket.listen(1)
 print(f"Server berjalan di {HOST}:{PORT}")
 
 while True:
-    # Menerima koneksi dari klien
+    # Jika ada request connection maka menerima koneksi dari klien
     client_socket, client_address = server_socket.accept()
-    print(f"Terhubung dari {client_address[0]}:{client_address[1]}")
 
     try:
-        # Menerima data dari klien
+        # Membaca request dari link browser client
         request = client_socket.recv(1024).decode()
-        # Memproses permintaan klien
+        # Membaca nama file yang ada dari request
         filename = request.split()[1][1:]
+        # Membuka file dan menyimpan ke dalam variabel file
         file = open(filename, "r")
+        # Membaca dan menyimpan file yang telah dibuka ke variabel file_content lalu selanjutnya file ditutup
         file_contents = file.read()
         file.close()
+        # Membuat header agar file dapat dibaca oleh browser
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + file_contents
+        # Mengirimkan isi dari file pada browser client
         for i in range(0, len(response)):
             client_socket.send(response[i].encode())
         client_socket.send("\r\n".encode())
         client_socket.close()
+    # Apabila file tidak terdapat pada data system maka lanjut ke except
     except IOError:
+        # Sama seperti diatas tetapi file yang dibaca hanya file 404.html tidak menggunakan request message
         file = open("404.html", "r")
         file_404 = file.read()
         file.close()
